8959f2f64221113d57f30aa0e611e54e90302e77
sethpalmer94 <sethpalmer@mail.weber.edu>
2017-10-12 12:10:02 -0600

    new program

--- module 7/casting.cpp
+++ module 7/casting.cpp
@@ -5,49 +5,63 @@ using namespace std;
 class Distance
 {
 private:
+	 float MTF; // meters to feet 
 	int feet;
 	float inches;
 public:
-	Distance() :feet(0), inches(0.0)
+	Distance() :feet(0), inches(0.0), MTF(3.280833F)
 	{
 
 	}
-	Distance(int f, float i)
+	Distance(int meters): MTF(3.280833F)
 	{
-		feet = f;
-		inches = i;
+		float fltfeet = MTF * meters;
+		feet = int(fltfeet);
+		inches = 12*(fltfeet-feet);
 
+	}
+	Distance(int f, float i):MTF(3.280833f)
+	{
+		inches = i; 
+		feet = f;
 	}
 	void getDist()
 	{
 		cout << " \nEnter feet: " << endl;
 		cin >> feet;
 		cout << "\nEnter inches:" << endl;
-		cin >> inches; 
+		cin >> inches;
 	}
 	void showDist()
 	{
 		cout << "feet: " << feet << " inches: " << inches << endl;
 	}
 	Distance operator + (Distance) const;
-	void operator += (Distance) ;
-	
+	void operator += (Distance);
+	operator float() const // convert to inches 
+	{
+		float fFeet = inches / 12;
+		fFeet += static_cast<float>(feet);
+		return fFeet / MTF;
+
+	}
+
 };
 Distance Distance::operator+(Distance d2) const
 {
 	int f = feet + d2.feet;
-	float i = inches +  d2.inches;
-	while(i > 12 )
+	float i = inches + d2.inches;
+	while (i > 12)
 	{
 		f++;
 		i - 12;
 	}
-	return Distance (f,i);
+	return Distance(f, i);
 }
 void Distance::operator+=(Distance d2)
 {
-     feet = feet + d2.feet;
-    inches = inches + d2.inches;
+	feet = feet + d2.feet;
+	inches = inches + d2.inches;
 	while (inches > 12)
 	{
 		feet++;
@@ -58,9 +72,9 @@ void Distance::operator+=(Distance d2)
 }
 int main()
 {
-	Distance d1;
-	Distance d2(11, 6.25);
-	Distance d3(22, 2.1);
+	Distance d1;//1st constructor
+	Distance d2(11);//second construtor
+	Distance d3(22, 2.1);//third
 	cout << "Distance " << endl;
 	d1.showDist();
 	d1 = d1 + d2;
@@ -76,7 +90,7 @@ int main()
 
 
 
-cout << "press any key to close" << endl;
-_getch();
-return 0;
+	cout << "press any key to close" << endl;
+	_getch();
+	return 0;
 }
